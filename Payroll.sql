22b18945500f093546d96302e023503471f6fdaa
JulietNwaka <juliet.nwaka.egbule@gmail.com>
2024-07-24 06:56:49 +0200

    Add files via upload

---
+++ Payroll.sql
@@ -0,0 +1,116 @@
+-- please rename the table from workers to employees
+ALTER TABLE workers
+RENAME TO employees
+
+-- find the highest salary earner 
+SELECT * FROM employees
+ORDER BY Salary Desc
+Limit 1
+
+-- find the lowest salary earner
+SELECT * FROM employees
+ORDER BY Salary ASC
+Limit 1
+
+-- list out all workers in IT
+SELECT * 
+FROM employees
+WHERE Department IN ('IT')
+
+-- what is the total number of workers in Bauchi state
+SELECT COUNT(*)
+FROM employees
+WHERE State = 'Bauchi'
+
+-- count numbers of employees in every state
+SELECT State, COUNT(State)
+FROM employees
+group by 1
+
+-- which state highest workers shortage 
+SELECT State, COUNT(State)
+FROM employees
+GROUP BY 1
+ORDER BY 2
+limit 1
+
+-- when was the last hiring date
+SELECT MAX(HireDate)
+FROM employees
+
+-- what was the total salary and bonus paid to each department
+SELECT Department,SUM(Salary),SUM(Bonus)
+FROM employees
+GROUP BY 1
+
+-- get the total income by adding salary and bonus
+SELECT Department, (Salary + Bonus) AS TotalIncome
+FROM employees
+
+-- calculate the total bonus percentage
+SELECT SUM(Bonus)/SUM(Salary) AS Bonus%
+FROM employees
+
+-- get the list of all employees that earns above 5000
+SELECT * 
+FROM employees
+WHERE Salary > 5000
+
+-- get all employees that earn more than total average salary
+SELECT *
+FROM employee
+WHERE Salary > (SELECT AVG(Salary) FROM employees)
+
+
+-- how many employees in Lagos state earn more than 250000
+SELECT COUNT(*) 
+FROM employees
+WHERE Salary > 250000
+AND State = 'Lagos'
+
+-- get the list of employees that joined the company in 2023
+SELECT *
+FROM employee
+WHERE EXTRACT(YEAR from HireDate) = 2023
+
+-- get the highest and the lowest salary paid in all state
+SELECT
+State, MAX(Salary) AS Highest , MIN(Salary) AS Lowest
+FROM employees
+Group by 1
+
+-- create a new column as Company, then insert JulieChan.AG in all rows 
+ALTER TABLE employees
+ADD Company varchar(20)
+UPDATE employees
+SET Company = 'Juliechan.com'
+
+
+-- how many male and female workers do we have?
+SELECT
+Gender, COUNT(Gender)
+FROM employees
+GROUP BY 1
+
+-- create a new column showing employee status. all employees below 61 as active while those above 60 as retired
+SELECT Age,
+ CASE
+   WHEN Age < 61 THEN 'Active Worker'
+ ELSE 'Retired'
+ END AS EmployeeStatus
+ FROM employees
+
+
+ -- How many countries do our workers come from?
+ SELECT COUNT(DISTINCT Nationality) FROM employees
+
+-- list all the the unique countries 
+SELECT Nationality,  DISTINCT(Nationality)
+ FROM employees
+ GROUP BY 1
+
+-- women conference is taking place next week and we need to reach out to all female workers
+-- get all female workers phone number
+SELECT PhoneNumber
+FROM workers
+WHERE Gender = 'Female'
